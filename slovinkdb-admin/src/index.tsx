4394271029c5bb6d6fbea573ff1c3e71d92dc017
Krzysztof M <marecki.krzysztof@gmail.com>
2018-01-13 00:42:43 +0100

    Add admin react app

---
+++ slovinkdb-admin/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>SlovnikDB admin</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
